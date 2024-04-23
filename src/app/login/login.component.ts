3545b8473fb7001bbbf11a0f82c9be37029fb894
Luca Mann <luca.mann@uni-konstanz.de>
2024-04-23 21:13:41 +0200

    Started implementing Log-In Form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import { ButtonModule } from "primeng/button";
+import { CommonModule } from "@angular/common";
+import { FloatLabelModule } from 'primeng/floatlabel';
+
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [],
+    imports: [ReactiveFormsModule, InputTextModule, ButtonModule, CommonModule, FloatLabelModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+
+        loginForm = new FormGroup({
+            email: new FormControl ('', [
+                Validators.required,
+                Validators.email
+            ]),
+            password: new FormControl ('', [
+                Validators.required,
+            ])
+        });
+password: any;
+
+        get email(){
+            return this.loginForm.get('email');
+        }
+
+        onSubmit() {
+            if (this.loginForm.invalid) {
+                
+            }
+        }
 }
