848d4c76ded45aeb8911df209527fe3b1f8c5800
luca.mann <luca.mann@uni-konstanz.de>
2024-04-24 16:24:08 +0200

    started register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from "primeng/button";
+import { CommonModule } from "@angular/common";
+import { FloatLabelModule } from 'primeng/floatlabel';
+
+import { NgForm } from '@angular/forms';
+
 
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+    selector: 'app-register',
+    standalone: true,
+    imports: [ReactiveFormsModule, InputTextModule, ButtonModule, CommonModule, FloatLabelModule],
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent {
 
+  registerForm = new FormGroup({
+  
+    email: new FormControl('', [
+        Validators.required,
+        Validators.email
+    ]),
+    password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+    ])
+  });
+  
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+
+    }
+  }
+
+  
 }
