5256cd122167b4b8dc9cb1fd6da2043eb81981be
Thư <1759038@itec.hcmus.edu.vn>
2024-04-27 20:18:19 +0200

    ThuNguyen/fix calendar and country

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,19 +4,24 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from "primeng/button";
 import { CommonModule } from "@angular/common";
 import { FloatLabelModule } from 'primeng/floatlabel';
-
+import { CalendarModule } from 'primeng/calendar';
+import {DropdownModule} from 'primeng/dropdown';
 import { NgForm } from '@angular/forms';
 
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [ReactiveFormsModule, InputTextModule, ButtonModule, CommonModule, FloatLabelModule],
+    imports: [ReactiveFormsModule, InputTextModule, ButtonModule, CommonModule, FloatLabelModule,CalendarModule,DropdownModule],
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.css']
 })
 
 export class RegisterComponent {
+
+  countries = [{name: "USA"}, {name:"New Zealand"},{name: "Germany"},{name: "Vietnam"}];
+
+  
 
   registerForm = new FormGroup({
   
@@ -27,9 +32,10 @@ export class RegisterComponent {
     password: new FormControl('', [
         Validators.required,
         Validators.minLength(8),
-    ])
+    ]),
+    
   });
-  
+
 
   onSubmit() {
     if (this.registerForm.invalid) {
