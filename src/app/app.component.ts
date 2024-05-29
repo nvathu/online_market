275e5d214c79e04f0b1ca3719850da1624e606d6
luca.mann <luca.mann@uni-konstanz.de>
2024-05-29 23:44:45 +0200

    changed logic of filters and search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MenubarComponent } from './menubar/menubar.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './product-list/product-list.component';
+import { FilterSidebarComponent } from './filter-sidebar/filter-sidebar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, RouterLink, RouterLinkActive, MenubarComponent],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, MenubarComponent,
+        FormsModule,
+        HttpClientModule,],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
