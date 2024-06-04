92e8f9e8052de94a32c73bfaa4b925be40d5d979
Luca Mann <luca.mann@uni-konstanz.de>
2024-06-04 11:17:50 +0200

    searchbar Update

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,35 +7,41 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {ProgressSpinnerModule} from 'primeng/progressspinner'
-import { ActivatedRoute,Params,RouterLink } from '@angular/router';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ActivatedRoute, Params, RouterLink } from '@angular/router';
 import { FilterSidebarComponent } from "../filter-sidebar/filter-sidebar.component";
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { InputGroupModule } from 'primeng/inputgroup';
-
-
 import { HttpClientModule } from '@angular/common/http';
-
-
-
-
 import { ToolbarModule } from 'primeng/toolbar';
-
 import { SplitButtonModule } from 'primeng/splitbutton';
 
 @Component({
-    selector: 'app-product-list',
-    standalone: true,
-    templateUrl: './product-list.component.html',
-    styleUrl: './product-list.component.css',
-    imports: [CommonModule, ProductItemComponent, MatIconModule, MatButtonModule, MatProgressSpinnerModule, ProgressSpinnerModule, RouterLink, ProductDetailComponent, FilterSidebarComponent,
-      FormsModule, InputTextModule, ButtonModule, InputGroupModule, HttpClientModule, ToolbarModule, SplitButtonModule
-    ]
+  selector: 'app-product-list',
+  standalone: true,
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  imports: [
+    CommonModule,
+    ProductItemComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    ProgressSpinnerModule,
+    RouterLink,
+    ProductDetailComponent,
+    FilterSidebarComponent,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    InputGroupModule,
+    HttpClientModule,
+    ToolbarModule,
+    SplitButtonModule,
+  ],
 })
-
-
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
   allProducts: Product[] = [];
@@ -49,12 +55,12 @@ export class ProductListComponent implements OnInit {
 
   selectedBrands: string[] = [];
   selectedColors: string[] = [];
-  selectedPriceRange: [number, number] = [0, 1000];  // Beispielwerte für den Preisbereich
+  selectedPriceRange: [number, number] = [0, 1000]; // Beispielwerte für den Preisbereich
 
   constructor(private productService: ProductService) {
     this.items = [
       { label: 'Update', icon: 'pi pi-refresh', command: () => this.updateProducts() },
-      { label: 'Delete', icon: 'pi pi-times', command: () => this.deleteProducts() }
+      { label: 'Delete', icon: 'pi pi-times', command: () => this.deleteProducts() },
     ];
   }
 
@@ -64,7 +70,7 @@ export class ProductListComponent implements OnInit {
 
   loadInitialProducts(): void {
     this.loading = true;
-    this.productService.getAllProductMetadata().subscribe(products => {
+    this.productService.getAllProductMetadata().subscribe((products) => {
       this.allProducts = products;
       this.searchProducts();
       this.loading = false;
@@ -73,7 +79,7 @@ export class ProductListComponent implements OnInit {
   }
 
   searchProducts(): void {
-    this.searchResults = this.allProducts.filter(product => this.filterProducts(product));
+    this.searchResults = this.allProducts.filter((product) => this.filterProducts(product));
     this.nextIndex = 0;
     this.products = this.searchResults.slice(this.nextIndex, this.nextIndex + 25);
     this.nextIndex += 25;
@@ -81,17 +87,18 @@ export class ProductListComponent implements OnInit {
 
   filterProducts(product: Product): boolean {
     const searchTextLower = this.searchText.toLowerCase();
-    const matchesSearchText = product.name.toLowerCase().includes(searchTextLower) ||
-           product.brand.toLowerCase().includes(searchTextLower) ||
-           product.colors.some(color => color.color_name.toLowerCase().includes(searchTextLower)) ||
-           product.category.toLowerCase().includes(searchTextLower) ||
-           product.type.toLowerCase().includes(searchTextLower) ||
-           product.gender.toLowerCase().includes(searchTextLower);
-    
+    const matchesSearchText =
+      product.name.toLowerCase().includes(searchTextLower) ||
+      product.brand.toLowerCase().includes(searchTextLower) ||
+      product.colors.some((color) => color.color_name.toLowerCase().includes(searchTextLower)) ||
+      product.category.toLowerCase().includes(searchTextLower) ||
+      product.type.toLowerCase().includes(searchTextLower) ||
+      product.gender.toLowerCase().includes(searchTextLower);
+
     const matchesBrand = this.selectedBrands.length ? this.selectedBrands.includes(product.brand) : true;
-    const matchesColor = this.selectedColors.length ? product.colors.some(color => this.selectedColors.includes(color.color_name)) : true;
+    const matchesColor = this.selectedColors.length ? product.colors.some((color) => this.selectedColors.includes(color.color_name)) : true;
     const matchesPrice = product.price >= this.selectedPriceRange[0] && product.price <= this.selectedPriceRange[1];
-    
+
     return matchesSearchText && matchesBrand && matchesColor && matchesPrice;
   }
 
@@ -129,7 +136,7 @@ export class ProductListComponent implements OnInit {
     // Logik zum Löschen der Produkte
   }
 
-  onFiltersChanged(filters: {brands: string[], colors: string[], priceRange: [number, number]}): void {
+  onFiltersChanged(filters: { brands: string[]; colors: string[]; priceRange: [number, number] }): void {
     this.selectedBrands = filters.brands;
     this.selectedColors = filters.colors;
     this.selectedPriceRange = filters.priceRange;
